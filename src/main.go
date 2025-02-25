1b5f6f390fda3508f1373a3428cf721b9625e935
Omar Flores <dev@dev.com>
2025-02-25 07:13:15 -0800

    add HTTP handlers for authors and books in main application

--- src/main.go
+++ src/main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"go-books/src/controllers"
+	"net/http"
 )
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hola Mundo")
+	fmt.Fprintln(w, "Hola Mundo")
 }
 
 func main() {
-    http.HandleFunc("/", helloWorldHandler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", helloWorldHandler)
+
+	http.HandleFunc("/authors", controllers.GetAuthors)
+	http.HandleFunc("/author", controllers.GetAuthorByID)
+	http.HandleFunc("/author/create", controllers.CreateAuthor)
+	http.HandleFunc("/author/update", controllers.UpdateAuthor)
+	http.HandleFunc("/author/delete", controllers.DeleteAuthor)
+
+	http.HandleFunc("/books", controllers.GetBooks)
+	http.HandleFunc("/book", controllers.GetBookByID)
+	http.HandleFunc("/book/create", controllers.CreateBook)
+	http.HandleFunc("/book/update", controllers.UpdateBook)
+	http.HandleFunc("/book/delete", controllers.DeleteBook)
+
+	http.ListenAndServe(":8080", nil)
 }
